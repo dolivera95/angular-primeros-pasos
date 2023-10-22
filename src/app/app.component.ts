c4b8914ef52138060c524ca9b602e1df5ff83e57
Daniel <dolivera95@gmail.com>
2023-10-22 12:44:55 -0500

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title: string = 'Hola mundo';
+  public counter: number = 10;
+
+  increaseBy(value:number):void{
+    this.counter = this.counter + value;
+  }
+
+  resetBut():void{
+    this.counter = 10;
+  }
+
+}
---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalizedName():string {
+    return this.name.toUpperCase()
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero():void{
+    this.name = 'Spiderman';
+  }
+
+  changeAge():void{
+    this.age = 28;
+  }
+
+  resetForm():void{
+    //this.name = 'ironman';
+    this.age = 45;
+    /*
+    document.querySelector('h1')!.innerHTML = '<h1>Desge Angular</h1>';
+
+    document.querySelectorAll('h1')!.forEach(elemento => {
+      elemento.innerHTML = '<h1>Desge Angular</h1>';
+    })
+    */
+  }
+
+
+
+}
